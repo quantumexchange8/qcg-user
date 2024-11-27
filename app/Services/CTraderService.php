783089fd4a161d3d4401aa6a6d9e783d592d4638
jrong04 <143994205+jrong04@users.noreply.github.com>
2024-11-27 17:58:41 +0800

    update on leverage again

--- app/Services/CTraderService.php
+++ app/Services/CTraderService.php
@@ -145,7 +145,6 @@ class CTraderService
     {
         $tradingUser =  TradingUser::firstWhere('meta_login', $meta_login);
         $response = Http::acceptJson()->patch($this->baseURL . "/v2/webserv/traders/$meta_login?token=$this->token", [
-            'login' => $meta_login,
             'groupName' => $tradingUser->meta_group,
             'leverageInCents' => $leverage * 100,
         ]);
