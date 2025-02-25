3c6863d27ce6e99a4397563e02b449486904b2da
jrong04 <143994205+jrong04@users.noreply.github.com>
2025-02-25 10:53:13 +0800

    temp fix: last_access

--- app/Services/Data/UpdateTradingUser.php
+++ app/Services/Data/UpdateTradingUser.php
@@ -29,16 +29,35 @@ class UpdateTradingUser
         $tradingUser->leverage = $data['leverageInCents'] / 100;
         $tradingUser->registration = $data['registrationTimestamp'];
 
-        if (isset($data['lastConnectionTimestamp'])) {
-            $tradingUser->last_access = Carbon::createFromTimestamp($data['lastConnectionTimestamp'] / 1000)->toDateTimeString();
-        }
-
-        $tradingUser->balance = $data['balance'] / 100;
-        $tradingUser->credit = $data['nonWithdrawableBonus'] / 100;
-
-        DB::transaction(function () use ($tradingUser) {
+        try {
+            if (isset($data['lastConnectionTimestamp'])) {
+                $timestamp = (int) $data['lastConnectionTimestamp'];
+    
+                if ($timestamp > 9999999999) {
+                    $timestamp = $timestamp / 1000;
+                }
+    
+                // $tradingUser->last_access = Carbon::createFromTimestamp($timestamp)->toDateTimeString();
+                $tradingUser->forceFill(['last_access' => Carbon::createFromTimestamp($timestamp)->toDateTimeString()]); // Laravel Eloquent forcechange
+                Log::info("Refreshing last access for account {$meta_login} to {$tradingUser->last_access}");
+            } else {
+                $tradingUser->last_access = null;
+            }
+            
+            $tradingUser->balance = $data['balance'] / 100;
+            $tradingUser->credit = $data['nonWithdrawableBonus'] / 100;
+            Log::info('Dirty attributes:', $tradingUser->getDirty());
+    
             $tradingUser->save();
-        });
+    
+            if ($tradingUser->wasChanged('last_access')) {
+                Log::info("Successfully updated last_access for {$tradingUser->meta_login}");
+            } else {
+                Log::warning("Warning: last_access did not change for {$tradingUser->meta_login}");
+            }
+        } catch (\Exception $e) {
+            Log::error("Failed to update last_access for {$tradingUser->meta_login}: " . $e->getMessage());
+        }
 
         return $tradingUser;
     }
