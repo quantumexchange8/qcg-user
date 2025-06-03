c3bdbeec6d3de806514b12ee34e00f02c43169d2
jrong04 <143994205+jrong04@users.noreply.github.com>
2025-06-03 15:24:08 +0800

    fix: dialog-sm (mobile) too large (vh)

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -338,7 +338,7 @@ const responsiveOptions = ref([
 
     <Dialog v-model:visible="showPopup" modal :header="$t('public.announcement')" class="dialog-xs md:dialog-md no-header-border" :closable="false">
         <div class="flex flex-col justify-center items-start gap-8 pb-6 self-stretch">
-            <img :src="currentAnnouncement.thumbnail" alt="announcement_image" class="w-full h-[144px] md:h-[310.5px]" />
+            <img :src="currentAnnouncement.thumbnail" alt="announcement_image" class="w-full h-[170px] md:h-[310.5px]" />
 
             <span class="text-lg font-bold text-gray-950">{{ currentAnnouncement.title }}</span>
 
